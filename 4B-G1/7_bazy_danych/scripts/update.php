0a1c4b9ead50e72be8e85053cb31c8e51569bc0c
unknown <dziuba.marcin@uczen.zsk.poznan.pl>
2021-11-23 15:11:47 +0100

    23.11.2021

--- 4B-G1/7_bazy_danych/scripts/update.php
+++ 4B-G1/7_bazy_danych/scripts/update.php
@@ -6,13 +6,16 @@
       }
     }
     require_once './connect.php';
+
     $sql="UPDATE `users` SET `cityid` = '$_POST[cityid]', `name` = '$_POST[name]', `surname` = '$_POST[surname]', `birthday` = '$_POST[birthday]' WHERE `users`.`id` = '$_GET[id]';";
     $connect->query($sql);
+
     if ($connect->affected_rows == 1) {
       header("location: ../5_bazy_tabela_update.php?error=2&info=Prawidłowo zaktualizowano użytkownika!");
     }
     else {
       header("location: ../5_bazy_tabela_update.php?error=2&info=Nie zaktualizowano użytkownika!&updateUser=$_GET[id]");
     }
+    
     $connect->close();
 ?>
